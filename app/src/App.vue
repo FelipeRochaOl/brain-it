ec1d40918ce9af0892c8c638e95287ce5d6c3bbc
Felipe Rocha <feliperochaoliveira@gmail.com>
2023-04-16 16:29:35 -0300

    [TEST] Config template

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,30 +1,37 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <header>
+    <img alt="Vue logo" src="./assets/logo.png" />
+  </header>
   <router-view />
 </template>
 
+<script lang="ts">
+import M from "materialize-css";
+import "materialize-css/dist/css/materialize.min.css";
+
+export default {
+  name: "app",
+  mounted: function () {
+    M.AutoInit();
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+button {
+  border: 0;
+  background: none;
+  cursor: pointer;
+  margin: 2px;
 }
 
-nav {
-  padding: 30px;
+header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+form {
+  width: 100%;
 }
 </style>
