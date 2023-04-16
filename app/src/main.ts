51fd3311a7759a349d93e8b7dcf147fea3f7ba9b
Felipe Rocha <feliperochaoliveira@gmail.com>
2023-04-16 16:30:33 -0300

    [TEST] Add fontawesome, jquery, moshaToast

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,7 +1,28 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faFloppyDisk,
+  faPen,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import moshaToast from "mosha-vue-toastify";
 import { createApp } from "vue";
+
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+/* add jquery */
+import $ from "jquery";
+window.jQuery = window.$ = $;
+
+/* add icons to the library */
+library.add(faPen, faTrash, faFloppyDisk);
+
+createApp(App)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .use(moshaToast)
+  .use(store)
+  .use(router)
+  .mount("#app");
